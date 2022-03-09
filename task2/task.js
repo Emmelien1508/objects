eea6845009217b57a0516bd2e63a256fb1a65a30
Emmelien1508 <emmelienschiet15@gmail.com>
2022-03-10 00:25:50 +0100

    Completed blackjack

--- task2/task.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    var game = new Game();
-
-    document.querySelector("#deal").addEventListener("click", () => game.deal());
-    document.querySelector("#hit").addEventListener("click", () => game.hit());
-    document.querySelector("#stand").addEventListener("click", () => game.stand());
-    document.querySelector("#reset").addEventListener("click", () => game.reset());
-})
--- task2/task2.js
+++ task2/task2.js
@@ -1,8 +1,140 @@
+var suits = {
+    "diamond": "&diams;",
+    "heart": "&hearts;",
+    "spade": "&spades;",
+    "club": "&clubs;"
+}
+var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king", "ace"]
+
+class Game {
+    constructor() {
+        this.deck = new Deck();
+        this.player = new Player("player", true);
+        this.dealer = new Player("dealer", false);
+        this.deck.createNewDeck();
+        this.deck.shuffle();
+        this.firstDeal();
+        this.showCardsOnTable(false);
+        this.showInitialScores();
+    }
+    showCardsOnTable(reset=true) {
+        let tables = document.querySelectorAll(".table");
+        tables.forEach((table) => {
+            if (reset) {
+                table.innerHTML = "";
+            }
+            const isPlayer = (table.dataset.player ==='true');
+            let person = isPlayer ? this.player : this.dealer;
+            person.cards.forEach((card) => {
+                let htmlText = '<div class="card flex col align-center justify-center';
+                htmlText += card.show ? '">' : ' secret">'
+                htmlText += `${card.value} ${suits[card.suit]}</div>`
+                table.innerHTML += htmlText;
+            })
+        })
+    }
+    showInitialScores() {
+        let scores = document.querySelectorAll(".score");
+        scores.forEach((score) => {
+            const isPlayer = (score.dataset.player === 'true');
+            score.innerText = isPlayer ? this.player.calculateScore() : "?";
+            this.dealer.calculateScore();
+        })
+    }
+    firstDeal() {
+        this.player.getCard(this.deck.deal());
+        this.dealer.getCard(this.deck.deal(false));
+        this.player.getCard(this.deck.deal());
+        this.dealer.getCard(this.deck.deal());
+    }
+    checkFor21() {
+        // check if player or dealer has blackjack
+        return this.player.calculateScore() == 21 || this.dealer.calculateScore() == 21 ? this.isThereAWinner() : false;
+    }
+    isThereAWinner() {
+        return this.player.calculateScore() == this.dealer.calculateScore() ? false : true;
+    }
+    whoWins() {
+        // get the winner of the game
+        if (this.player.calculateScore() == this.dealer.calculateScore()) {
+            return "It's a tie!";
+        } else if (this.dealer.calculateScore() > 21) {
+            return "You win!";
+        } else if (this.dealer.calculateScore() > this.player.calculateScore()) {
+            return "Dealer wins :(";
+        } else {
+            return "You win! :)";
+        }
+    }
+    hit() {
+        this.dealCardToPlayer();
+        if (this.player.calculateScore() > 21) {
+            this.disableButtons();
+            this.dealer.revealHiddenCard();
+            document.querySelector(".dealer-score").innerText = this.dealer.calculateScore();
+            document.querySelector(".result").innerHTML = "You lose :(";
+        } else {
+            this.dealCardToDealer();
+        }
+    }
+    stand() {
+        this.disableButtons();
+        this.dealer.revealHiddenCard();
+        document.querySelector(".dealer-score").innerText = this.dealer.calculateScore();
+        let winner = this.whoWins();
+        document.querySelector(".result").innerHTML = winner;
+    }
+    reset() {
+        document.querySelector(".result").innerHTML = "";
+        this.enableButtons();
+        this.player.cards = [];
+        this.dealer.cards = [];
+
+        this.deck.cards = [];
+        this.deck.createNewDeck();
+        this.deck.shuffle();
+
+        this.firstDeal();
+        this.showCardsOnTable();
+        this.showInitialScores();
+
+        if (this.checkFor21()) {
+            let winner = this.whoWins();
+            document.querySelector(".result").innerHTML = winner;
+        };
+    }
+    dealCardToPlayer() {
+        let cardDealt = this.deck.deal();
+        this.player.getCard(cardDealt);
+        this.player.updateScore(cardDealt);
+        document.querySelector(".player-cards").innerHTML += `<div class="card flex col align-center justify-center">${cardDealt.value} ${suits[cardDealt.suit]}</div>`
+        document.querySelector(".player-score").innerText = this.player.calculateScore();
+    }
+    dealCardToDealer() {
+        let secondCardDealt = this.deck.deal();
+        this.dealer.getCard(secondCardDealt);
+        this.dealer.updateScore(secondCardDealt);
+        document.querySelector(".dealer-cards").innerHTML += `<div class="card flex col align-center justify-center">${secondCardDealt.value} ${suits[secondCardDealt.suit]}</div>`
+    }
+    disableButtons() {
+        document.querySelector("#hit").classList.add("disabled");
+        document.querySelector("#stand").classList.add("disabled");
+        document.querySelector("#hit").setAttribute("disabled", true);
+        document.querySelector("#stand").setAttribute("disabled", true);
+    }
+    enableButtons() {
+        document.querySelector("#hit").classList.remove("disabled");
+        document.querySelector("#stand").classList.remove("disabled");
+        document.querySelector("#hit").removeAttribute("disabled");
+        document.querySelector("#stand").removeAttribute("disabled");
+    }
+}
+
 class Card {
     constructor(suit, value) {
         this.suit = suit;
         this.value = value;
-        this.shown = true;
+        this.show = true;
     }
     getNumericValue(aceValue) {
         const faceOrAce = this.value === "ace" ? aceValue : 10;
@@ -14,12 +146,28 @@ class Deck {
     constructor() {
         this.cards = [];
     }
+    createNewDeck() {
+        values.forEach((value) => {
+            for (let suit in suits) {
+                this.cards.push(new Card(suit, value));
+            }
+        })
+    }
     shuffle() {
-        shuffleCards(this.cards);
-    }   
-    deal(shown=true) {
+        for (var i = this.cards.length - 1; i > 0; i--) {
+            // Generate random number
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = this.cards[i];
+            this.cards[i] = this.cards[j];
+            this.cards[j] = temp;
+        }
+    }
+    deal(show=true) {
         let dealtCard = this.cards.pop();
-        shown ? "" : dealtCard.shown = false;
+        if (!show) {
+            dealtCard.show = false;
+        }
+        // hier wat dingen doen voor de html dingen? om te laten zien?
         return dealtCard;
     }
 }
@@ -28,163 +176,31 @@ class Player {
     constructor(name, turn) {
         this.name = name;
         this.cards = [];
-        this.score = 0;
-        this.turn = turn;
+        this.isTurn = turn;
     }
     getCard(card) {
         this.cards.push(card);
     }
-    hit() {
-        if (this.name === "dealer") {
-            return this.score < 17 ? true : false;
-        }
-        return true;
+    updateScore(cardDealt, aceValue=1) {
+        this.score += cardDealt.getNumericValue(aceValue);
     }
-    updateScore(card, aceValue=11) {
-        this.score += card.getNumericValue(aceValue);
+    revealHiddenCard() {
+        let hiddenCard = document.querySelector(".secret");
+        hiddenCard.classList.remove("secret");
     }
-    calculateScore(aceValue=11) {
+    calculateScore(aceValue=1) {
+        let score = 0;
         this.cards.forEach((card) => {
-            this.score += card.getNumericValue(aceValue);
+            score += card.getNumericValue(aceValue);
         })
-        return this.score;
+        return score;
     } 
-}
-
-function shuffleCards(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        // Generate random number
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-}
-
-var dealer = new Player("dealer", false);
-var player = new Player("player", true);
-var deck = new Deck();
-var suits = [];
-suits["diamond"] = "&diams;";
-suits["heart"] = "&hearts;";
-suits["spade"] = "&spades;";
-suits["club"] = "&clubs;";
-var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king", "ace"]
-
-values.forEach((value) => {
-    for (suit in suits) {
-        deck.cards.push(new Card(suit, value));
-    }
-})
-
-dealFirstCards();
-
-function dealFirstCards() {
-    deck.shuffle();
-    // player.getCard(deck.deal());
-    // dealer.getCard(deck.deal());
-    // player.getCard(deck.deal());
-    // dealer.getCard(deck.deal(shown=false));
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    // initCardsOnTable();
-    startGame();
+    var game = new Game();
+
+    document.querySelector("#hit").addEventListener("click", () => game.hit());
+    document.querySelector("#stand").addEventListener("click", () => game.stand());
+    document.querySelector("#reset").addEventListener("click", () => game.reset());
 })
-
-function initCardsOnTable() {
-    let tables = document.querySelectorAll(".table");
-    tables.forEach((table) => {
-        const isPlayer = (table.dataset.player ==='true');
-        let person = isPlayer ? player : dealer;
-        person.cards.forEach((card) => {
-            let htmlText = '<div class="card flex col align-center justify-center';
-            htmlText += card.shown ? '">' : ' secret">'
-            htmlText += `${card.value} ${suits[card.suit]}</div>`
-            table.innerHTML += htmlText;
-        })
-    })
-}
-
-function initScores() {
-    let scores = document.querySelectorAll(".score");
-    scores.forEach((score) => {
-        const isPlayer = (score.dataset.player === 'true');
-        score.innerText = isPlayer ? player.calculateScore() : "?";
-    })
-}
-
-function startGame() {
-    noWinnerYet = true
-    initScores();
-    
-
-    while (noWinnerYet) {
-        let turn = player.turn ? player : dealer;
-            document.querySelector(".turn").innerText = `It's ${turn.name}'s turn`;
-            if (player.turn) {
-                hitOrStand();
-            } else {
-                if (dealer.score < 17) {
-                    dealer.getCard(deck.deal());
-                    addCardToTable("dealer");
-                } else {
-                    checkWinner();
-                }
-            }
-        noWinnerYet = false;
-    }
-}
-
-function hitOrStand() {
-    document.querySelector("#hit").addEventListener("click", () => {
-        // do something here
-        player.getCard(deck.deal());
-        addCardToTable("player");
-        updateTableScore("player");
-        disableButtons();
-    })
-    
-    document.querySelector("#stand").addEventListener("click", () => {
-        // game ends
-        disableButtons();
-        checkWinner();
-    })
-    player.turn = false;
-}
-
-function checkWinner() {
-    let result;
-    if (player.score <= 21 && dealer.score <= 21) {
-        const notSameScore = player.score > dealer.score ? "Player won!" : "Dealer won!";
-        result = player.score == dealer.score ? "It's a tie!" : notSameScore;
-    } 
-    result = "There is no winner :(";
-    document.querySelector(".result").innerText = result;
-}
-
-function addCardToTable(whoseTurn) {
-    let turn = player.name === whoseTurn ? player : dealer
-    let table = document.querySelector(`.${whoseTurn}-cards`);
-    let card = turn.cards[turn.cards.length - 1];
-    table.innerHTML += `<div class="card flex col align-center justify-center">${card.value} ${suits[card.suit]}</div>`;
-}
-
-function updateTableScore(whoseTurn) {
-    let turn = player.name === whoseTurn ? player : dealer
-    let card = turn.cards[turn.cards.length - 1];
-    turn.updateScore(card);
-    let score = document.querySelector(".player-score");
-    score.innerText = turn.score;
-}
-
-function disableButtons() {
-    var buttons = document.querySelector(".decision-buttons").children;
-    for (button of buttons) {
-        button.classList.add("disabled");
-        button.disabled = true;
-    }
-}
-// based on current cards on the table, the player either clicks on "hit" or "stand" button
-    // if "hit" button is clicked, add another card to players deck
-    // else if "stand" button is clicked, dealer reveals hidden card and winner is decided
