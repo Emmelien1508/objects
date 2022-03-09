53259772c23c41d2102b30b7a810aaa92244fe54
Rens660 <rensjh@gmail.com>
2022-03-09 19:48:11 +0100

    stuff

--- task2/task.js
+++ task2/task.js
@@ -10,22 +10,32 @@ class Card {
     }
 }
 
+// suits["diamond"] = "&diams;";
+// suits["heart"] = "&hearts;";
+// suits["spade"] = "&spades;";
+// suits["club"] = "&clubs;";
+
 class Deck {
     constructor() {
         this.cards = [];
-        var suits = [];
-        suits["diamond"] = "&diams;";
-        suits["heart"] = "&hearts;";
-        suits["spade"] = "&spades;";
-        suits["club"] = "&clubs;";
+        var suits = ['diamonds', 'hearts', 'spades', 'clubs'];
         var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king", "ace"]
         values.forEach((value) => {
-            for (suit in suits) {
-                deck.cards.push(new Card(suit, value));
-            }
+            suits.forEach((suit) => {
+                this.cards.push(new Card(suit, value));
+            })
         })
-        shuffleCards(this.cards);
     }
+    shuffleDeck() {
+        for (var i = this.cards.length - 1; i > 0; i--) {
+            // Generate random number
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = this.array[i];
+            this.array[i] = this.array[j];
+            this.array[j] = temp;
+        }
+    }
+
     deal(shown=true) {
         let dealtCard = this.cards.pop();
         shown ? "" : dealtCard.shown = false;
@@ -33,31 +43,82 @@ class Deck {
     }
 }
 
-function shuffleCards(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        // Generate random number
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+class Player {
+    constructor(name, turn) {
+        this.name = name;
+        this.cards = [];
+        this.score = 0;
+        this.isTurn = turn;
     }
+    getCard(card) {
+        this.cards.push(card);
+    }
+    hit() {
+        if (this.name === "dealer") {
+            return this.score < 17 ? true : false;
+        }
+        return true;
+    }
+    updateScore(card, aceValue=11) {
+        this.score += card.getNumericValue(aceValue);
+    }
+
 }
+
+function checkWinner() {
+    let result;
+    if (player.score <= 21 && dealer.score <= 21) {
+        const notSameScore = player.score > dealer.score ? "Player won!" : "Dealer won!";
+        result = player.score == dealer.score ? "It's a tie!" : notSameScore;
+    } 
+    result = "There is no winner :(";
+    document.querySelector(".result").innerText = result;
+}
+
+function isThereAWinner(player) {
+    if (player.score > 21) return true
+    return false
+
+}
+
 
 document.addEventListener("DOMContentLoaded", () => {
     let noWinnerYet = true;
-    initScores();
 
-    var dealer = new Player("dealer", turn=false);
-    var player = new Player("player", turn=true);
+    var dealer = new Player("dealer", isTurn=false);
+    var player = new Player("player", isTurn=true);
     var deck = new Deck();
 
     while (noWinnerYet) {
         // check whose turn it is
+        let activePlayer = player.isTurn ? player : dealer;
+        let otherPlayer = player.isTurn ? dealer : player;
+
+        document.querySelector('.turn').innerText = `It's ${activePlayer.name} turn`
+
         // wait for choice 
-        // stop game or deal card
-        // add value to score 
+        document.querySelector('#hit').addEventListener('click', () => {
+            console.log("Player hits")
+            let dealtCard = deck.deal(true)
+            activePlayer.getCard(dealtCard)
+            activePlayer.updateScore(dealtCard)
+        })
+
+        document.querySelector('#stand').addEventListener('click', () => {
+            console.log("Player stands")
+        })
+
+        noWinnerYet = isThereAWinner(activePlayer) ? false : true;
+
+        if (noWinnerYet) {
+            activePlayer.isTurn = false
+            otherPlayer.isTurn = true
+        }
+
+
         // determine if winner 
         // give turn to other
         noWinnerYet = false;
     }
+
 })
